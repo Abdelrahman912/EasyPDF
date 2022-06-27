a3377aef89f4d116465f81e2aaad5860944f64f0
Fathy <abdelrahmanfathe95@gmail.com>
2022-06-27 05:23:36 +0200

    init projects

---
+++ EasyPDF/IO.cs
@@ -0,0 +1,22 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using System;
+
+namespace EasyPDF
+{
+    public static class IO
+    {
+        public static T WritePdf<T>(this string fileName,Func<Document,T> func)
+        {
+            using (var writer = new PdfWriter(fileName))
+            {
+                using (var pdf = new PdfDocument(writer))
+                {
+                    var doc = new Document(pdf);
+                    var result = func(doc);
+                    return result;
+                }
+            }
+        }
+    }
+}
